7a0b78e41f6d3b8789c9a3bbf3543ca0ad51b0f1
dushi27 <dushi27@gmail.com>
2014-08-07 16:24:10 +0000

    update page contents

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,8 +13,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def contact
-  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,4 @@ Twitter::Application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
 end
